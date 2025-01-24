0e20aca40d6a51d2b090c56cf07997105d9abb5b
vk <v.kukins@gmail.com>
2025-01-25 01:19:54 +0200

    build

---
+++ .output/public/_nuxt/2ucOLgbJ.js
@@ -0,0 +1,1 @@
+const e=()=>({legacy:!1,messages:{en:{welcome:"Welcome",hello:"Hello"},fr:{welcome:"Bienvenue",hello:"Bunjur"}}});export{e as default};
---
+++ .output/public/_nuxt/CZ3CQs_7.js
@@ -0,0 +1,1 @@
+import{_ as r,N as t}from"./DdGhY09T.js";const s={};function n(e,o){return t(e.$slots,"default")}const a=r(s,[["render",n]]);export{a as default};
