e2b35ecfe819965aa7e9e3e52b86f62e11c4e45b
vk <v.kukins@gmail.com>
2025-01-26 15:46:10 +0200

    indent and subline

---
+++ .output/public/_nuxt/B1rfkLz8.js
@@ -0,0 +1,1 @@
+import{_ as s,o as t,c as o,N as a}from"./uDO7ls3E.js";const c={},r={class:"wrapper"};function d(e,n){return t(),o("div",r,[a(e.$slots,"default",{},void 0)])}const f=s(c,[["render",d],["__scopeId","data-v-32418dfd"]]);export{f as default};
--- i18n.config.ts
+++ i18n.config.ts
@@ -3,7 +3,7 @@ export default defineI18nConfig(() => ({
   messages: {
     en: {
       '@main-slide$title':
-        'Breaking language barriers, building <span>business</span> bridges.',
+        '<i></i>Breaking language barriers, building <span>business</span> bridges.',
       '@main-slide$text':
         'We provide a full range of translation services to and from any language, helping our clients tackle business and legal challenges with confidence.',
       '@services-slide$title': 'Services',
