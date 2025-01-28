a16ed82bc7438a042b4271ce4cbeef7418fd1d36
vk <v.kukins@gmail.com>
2025-01-28 21:21:53 +0200

    seo

---
+++ .output/public/_nuxt/CerKNjot.js
@@ -0,0 +1,23 @@
+import{G as u,e as l,o as p,c,H as d,_ as m}from"./EWfaBC3m.js";import{u as n}from"./DZfOyMKf.js";function g(e,t){const{title:o,titleTemplate:s,...i}=e;return n({title:o,titleTemplate:s,_flatMeta:i},{...t,transform(a){const r=u({...a._flatMeta});return delete a._flatMeta,{...a,meta:r}}})}const j={class:"wrapper"},h=l({__name:"default",setup(e){return n({script:[{type:"text/javascript",innerHTML:`
+        ; (function (h, o, t, j, a, r) {
+          h.hj =
+            h.hj ||
+            function () {
+              ; (h.hj.q = h.hj.q || []).push(arguments)
+            }
+          h._hjSettings = { hjid: 3671350, hjsv: 6 }
+          a = o.getElementsByTagName('head')[0]
+          r = o.createElement('script')
+          r.async = 1
+          r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv
+          a.appendChild(r)
+        })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=')
+      `,tagPosition:"bodyClose"},{type:"text/javascript",src:"https://www.googletagmanager.com/gtag/js?id=G-SRB7S7MLJN",async:!0,tagPosition:"bodyClose"},{type:"text/javascript",innerHTML:`
+        window.dataLayer = window.dataLayer || []
+        function gtag() {
+          dataLayer.push(arguments)
+        }
+        gtag('js', new Date())
+
+        gtag('config', 'G-SRB7S7MLJN')
+      `,tagPosition:"bodyClose"}]}),g({ogUrl:'http://www.lingvojumi.lv"',title:"Lingvojumi · Tulkošanas birojs",ogTitle:"Lingvojumi · Tulkošanas birojs",description:"Mēs sniedzam pilnu tulkošanas pakalpojumu klāstu uz un no jebkuras valodas. Apostille, tulkošana un notariālie apliecinājumi.",ogDescription:"Mēs sniedzam pilnu tulkošanas pakalpojumu klāstu uz un no jebkuras valodas. Apostille, tulkošana un notariālie apliecinājumi.",ogImage:"https://kukin.me/og.png",twitterCard:"summary_large_image"}),(t,o)=>(p(),c("div",j,[d(t.$slots,"default",{},void 0)]))}}),k=m(h,[["__scopeId","data-v-5a6bd033"]]);export{k as default};
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,59 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+useHead({
+  script: [
+    {
+      type: 'text/javascript',
+      innerHTML: `
+        ; (function (h, o, t, j, a, r) {
+          h.hj =
+            h.hj ||
+            function () {
+              ; (h.hj.q = h.hj.q || []).push(arguments)
+            }
+          h._hjSettings = { hjid: 3671350, hjsv: 6 }
+          a = o.getElementsByTagName('head')[0]
+          r = o.createElement('script')
+          r.async = 1
+          r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv
+          a.appendChild(r)
+        })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=')
+      `,
+      tagPosition: 'bodyClose'
+    },
+    {
+      type: 'text/javascript',
+      src: 'https://www.googletagmanager.com/gtag/js?id=G-SRB7S7MLJN',
+      async: true,
+      tagPosition: 'bodyClose'
+    },
+    {
+      type: 'text/javascript',
+      innerHTML: `
+        window.dataLayer = window.dataLayer || []
+        function gtag() {
+          dataLayer.push(arguments)
+        }
+        gtag('js', new Date())
+
+        gtag('config', 'G-SRB7S7MLJN')
+      `,
+      tagPosition: 'bodyClose'
+    }
+  ]
+})
+
+useSeoMeta({
+  ogUrl: 'http://www.lingvojumi.lv"',
+  title: 'Lingvojumi · Tulkošanas birojs',
+  ogTitle: 'Lingvojumi · Tulkošanas birojs',
+  description:
+    'Mēs sniedzam pilnu tulkošanas pakalpojumu klāstu uz un no jebkuras valodas. Apostille, tulkošana un notariālie apliecinājumi.',
+  ogDescription:
+    'Mēs sniedzam pilnu tulkošanas pakalpojumu klāstu uz un no jebkuras valodas. Apostille, tulkošana un notariālie apliecinājumi.',
+  ogImage: 'https://kukin.me/og.png',
+  twitterCard: 'summary_large_image'
+})
+</script>
 
 <template>
   <div class="wrapper">
